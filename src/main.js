aeeb3b4f42a2c6461f57b2a482210d9850049e02
Sam <samuv.eth@gmail.com>
2021-01-02 15:09:07 +0700

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import Jazzicon from "./components";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component(Jazzicon.name, Jazzicon);
+
+app.mount("#app");
